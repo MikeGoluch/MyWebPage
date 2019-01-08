45d77e1c8d49fda38ba843a9b690d970d94ecfb4
Michal Goluch <goluch.michal89@gmail.com>
2019-01-08 18:01:44 +0100

    add mixins and variables, modify styles, add main font, change header image

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,47 @@
-$(document).ready(function () {
-    $('.third-button').on('click', function () {
+// $(document).ready(function () {
+//     $('.third-button').on('click', function () {
   
-      $('.animated-icon4').toggleClass('open');
+//       $('.animated-icon4').toggleClass('open');
+//     });
+//   });
+
+  $(document).ready(function () {
+    $('.toggle, .my-navbar-nav').click(function() {
+      $('.my-navbar').toggleClass('nav-is-open');
+      $('.toggle').toggleClass('active');
+      });
+      // window.addEventListener('load', function() {
+      //   let loadScreen = document.getElementById('load-screen');
+      //   document.body.removeChild(loadScreen);
+      // });
+      $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 0) {
+          $('.my-navbar').css('background-color' , 'rgba(0,0,0,.7)');
+        }
+    
+        else{
+          $('.my-navbar').css('background-color' , 'rgba(0,0,0,1)');  	
+        }
+      })
+
+      setTimeout(function(){
+        $('body').addClass('loaded');
+        // $('h1').css('color','#222222');
+      }, 6000);
+      
+      let i = 0;
+      function typeWriter() {
+        let txt = 'Welcome';
+        let speed = 300;
+        if (i < txt.length) {
+          document.getElementById('typeWriter').innerHTML += txt.charAt(i);
+          document.getElementById('lineExtend').style.width = (20 + (15 * i)) + 'px';
+          i++;
+          setTimeout(typeWriter,speed);
+        }
+      }
+      setTimeout(typeWriter, 3000);
     });
-  });
+
+
